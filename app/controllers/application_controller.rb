d7bbbfd8e49f04b814bb36f84915f4c8c715ad92
Olivia Li <oliviali.ouyang@gmail.com>
2021-12-28 23:09:50 -0500

    redirect to new page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,15 @@ class ApplicationController < ActionController::Base
 
   def login 
     if params[:password] == "nipples"
-      render "desktop"
+      session[:logged_in] = true
+      render json: { logged_in: true }
+    else
+      render json: { logged_in: false }
     end
   end
 
   def desktop
+    render layout: "application"
   end 
 
 end
--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,12 +1,19 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import axios from "axios";
 import api from "../helpers/api";
 
 const App = () => {
   const inputRef = useRef();
+  const [showError, setShowError] = useState();
+
   function login() {
     api.post("/login", { password: inputRef.current.value }).then((res) => {
-      console.log(res);
+      if (res.data.logged_in) {
+        window.location.href = "/desktop";
+        setShowError(false);
+      } else {
+        setShowError(true);
+      }
     });
   }
 
@@ -17,6 +24,7 @@ const App = () => {
         <input ref={inputRef} className="bg-blue-200 rounded mx-5 p-1" type="text" />
         <button onClick={() => login()}>Submit</button>
       </div>
+      {showError && <p className="text-red-500 text-center my-5">Wrong password</p>}
     </div>
   );
 };
