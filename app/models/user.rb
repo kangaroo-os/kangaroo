f50fcfdfaae7347478addbfcea8cdda77f559f6e
Olivia Li <oliviali.ouyang@gmail.com>
2021-12-29 14:16:29 -0500

    add users with devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_secure_password
-
-  validates_presence_of :email
-  validates_uniqueness_of :email
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
