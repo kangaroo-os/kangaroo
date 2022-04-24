403824b38593e941b2c952bae6f230c9802931a2
Olivia Li <oliviali.ouyang@gmail.com>
2022-04-23 20:07:44 -0700

    this might be bad code

--- app/javascript/components/shared/ContextMenu.tsx
+++ app/javascript/components/shared/ContextMenu.tsx
@@ -1,6 +1,26 @@
 import React, { ReactNode } from 'react'
 
 export const ContextMenu = ({ elements }: { elements: ReactNode }) => {
+  document.onclick = hideMenu
+  document.oncontextmenu = rightClick
+
+  function hideMenu() {
+    document.getElementById('contextMenu').style.display = 'none'
+  }
+
+  function rightClick(e) {
+    e.preventDefault()
+
+    if (document.getElementById('contextMenu').style.display == 'block') hideMenu()
+    else {
+      var menu = document.getElementById('contextMenu')
+
+      menu.style.display = 'block'
+      menu.style.left = e.pageX + 'px'
+      menu.style.top = e.pageY + 'px'
+    }
+  }
+
   return (
     <div id="contextMenu" className="context-menu absolute" style={{ display: 'none' }}>
       <ul>{elements}</ul>
