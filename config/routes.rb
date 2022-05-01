bb238ea9ffe44cccdac59dbb5591ba4b2cc935d2
kozr <zxzflow@gmail.com>
2022-04-30 21:05:09 -0700

    add cors stuff

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   post '/login', to: 'application#login'
   post '/authorized', to: 'application#authorized'
   post '/upload', to: 'files#upload'
-  get '/get_object', to: 'files#get_object'
+  get '/get_object', to: 'files#get_object', format: false
   get '/delete_object', to: 'files#delete_object'
   get '/files', to: 'files#get_files'
   get '*path', to: 'application#index'
