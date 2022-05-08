5806a5b26313ac0cd5eea8e81c915cce9fe69212
kozr <zxzflow@gmail.com>
2022-05-08 00:01:55 -0700

    handle with bearer token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :null_session, if: :json_request?
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def authorized
@@ -22,6 +23,10 @@ class ApplicationController < ActionController::Base
   end
 
   private
+
+  def json_request?
+    request.format.json?
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name])
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
-    resource '*', headers: :any, methods: %i[get]
+    origins 'localhost:3000', 'pegeejpeeihbcfnmaadambjckbgfcofc'
+    resource '*', headers: :any, methods: %i[get put post delete options]
   end
 end
