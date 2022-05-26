83480165de4cabba3b5ce7672cd3631efd53b8ce
kozr <zxzflow@gmail.com>
2022-05-25 23:38:17 -0700

    push fix

--- app/models/file_ownership.rb
+++ app/models/file_ownership.rb
@@ -2,6 +2,6 @@ class FileOwnership < ApplicationRecord
   belongs_to :user
   belongs_to :abstract_file
 
-  belongs_to :cloud_file, foreign_key: :abstract_file_id
-  belongs_to :link_file, foreign_key: :abstract_file_id
+  belongs_to :cloud_file, foreign_key: :abstract_file_id, optional: true
+  belongs_to :link_file, foreign_key: :abstract_file_id, optional: true
 end
