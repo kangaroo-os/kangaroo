bfd8f725c3c02d13bddd727208a0319f188e6dc7
Olivia Li <oliviali.ouyang@gmail.com>
2022-06-02 03:59:10 -0700

    add kangarooos

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000', 'pegeejpeeihbcfnmaadambjckbgfcofc', "pkhbmmbgbhhlglkfieijmjjnefgfpkmd"
+    origins 'localhost:3000', 'pegeejpeeihbcfnmaadambjckbgfcofc', "pkhbmmbgbhhlglkfieijmjjnefgfpkmd", 'kangarooos.com'
     resource '*',
       headers: :any,
       methods: %i[get put post delete options],
