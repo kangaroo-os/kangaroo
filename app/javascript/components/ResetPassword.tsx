afc3049d3f3791da134eb2df73c315ef34f3032c
Olivia Li <oliviali.ouyang@gmail.com>
2022-06-10 00:14:41 -0700

    make the reset password look good

--- app/javascript/components/ResetPassword.tsx
+++ app/javascript/components/ResetPassword.tsx
@@ -10,6 +10,10 @@ export const ResetPassword = () => {
   let navigate = useNavigate()
 
   useEffect(() => {
+    // if (!params['uid']) {
+    //   navigate('/signup')
+    //   return
+    // }
     const user = {
       id: null,
       email: params['uid'],
@@ -42,15 +46,40 @@ export const ResetPassword = () => {
   }
 
   return (
-    <div>
-      <h1>Reset your password</h1>
-      <form onSubmit={(e) => handleSubmit(e)}>
-        <label htmlFor="password">New password</label>
-        <input type="password" id="password" />
-        <label htmlFor="confirmPassword">Confirm password</label>
-        <input type="password" id="confirmPassword" />
-        <button type="submit">Reset password</button>
-      </form>
+    <div className="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+          <form className="space-y-6" onSubmit={(e) => handleSubmit(e)}>
+            <h1 className="text-lg">Reset your password</h1>
+            <div>
+              <label className="block text-sm font-medium text-gray-700" htmlFor="password">
+                New password
+              </label>
+              <input
+                className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                type="password"
+                id="password"
+              />
+            </div>
+            <div>
+              <label className="block text-sm font-medium text-gray-700" htmlFor="confirmPassword">
+                Confirm password
+              </label>
+              <input
+                className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                type="password"
+                id="confirmPassword"
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Reset password
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
