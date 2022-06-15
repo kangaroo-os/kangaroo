5c2c2666041fd145ab3d60ec884b26d67cc82a20
Olivia Li <oliviali.ouyang@gmail.com>
2022-06-15 04:02:50 -0700

    click link to save

--- app/javascript/components/shared/Window.tsx
+++ app/javascript/components/shared/Window.tsx
@@ -1,14 +1,17 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { useDesktop } from '@states/desktopState'
 import DroppableLocation from './desktop/DroppableLocation'
 import Toggle from '@components/shared/Toggle'
 import { makeFileShareable } from '@api/abstract_files'
 import { getDefaultPath } from '@helpers/fileStorage'
 import Draggable from 'react-draggable'
+import { sleep } from '@helpers/sleep'
 
 export const Window = ({ windowId, children }) => {
   const { closeWindow, desktop } = useDesktop()
-  const [isPublic, setIsPublic] = useState(!!desktop.fileMappings[windowId].public_share_url)
+  const shareRef = useRef<HTMLInputElement>()
+  const [showCopied, setShowCopied] = useState(false)
+  const [isPublic, setIsPublic] = useState(desktop.fileMappings[windowId].publicly_accessible)
   const [publicUrl, setPublicUrl] = useState(desktop.fileMappings[windowId].public_share_url || '')
 
   function handleCloseClick() {
@@ -33,6 +36,15 @@ export const Window = ({ windowId, children }) => {
     if (element) element.style.zIndex = `${maxZIndex + 1}`
   }
 
+  async function copyInputText() {
+    shareRef.current.select()
+    shareRef.current.setSelectionRange(0, 99999) /* For mobile devices */
+    navigator.clipboard.writeText(shareRef.current.value)
+    setShowCopied(true)
+    await sleep(2000)
+    setShowCopied(false)
+  }
+
   return (
     <Draggable handle={`#w-${randomId}`}>
       <div onMouseDownCapture={sendElementToTop} data-another-window={true} id={`window-${randomId}`} className="absolute top-[30%] left-[30%]">
@@ -45,10 +57,22 @@ export const Window = ({ windowId, children }) => {
             </div>
             <p className="ml-5">{desktop.fileMappings[windowId].name.replace(getDefaultPath(), '')}</p>
           </div>
-          <div className="absolute bottom-0 right-0 m-5 flex items-center space-x-2">
-            {isPublic && <input value={publicUrl} className="k-input-sm" readOnly />}
-            <Toggle onClick={(bool) => toggleCallback(bool)} enabled={!!desktop.fileMappings[windowId].public_share_url} />
-            <p>Public</p>
+          <div className="absolute bottom-0 right-0 m-5">
+            <div className="flex items-center space-x-2">
+              {isPublic && (
+                <input
+                  onClick={copyInputText}
+                  ref={shareRef}
+                  value={publicUrl}
+                  className="pl-7 k-input-sm cursor-pointer bg-gray-300 hover:bg-white hover:border-2 hover:border-blue-500"
+                  readOnly
+                />
+              )}
+              {showCopied && <i className="absolute fa-solid fa-circle-check text-green-700"></i>}
+              {!showCopied && <i className="absolute fa-regular fa-clipboard text-gray-500"></i>}
+              <Toggle onClick={(bool) => toggleCallback(bool)} enabled={!!desktop.fileMappings[windowId].publicly_accessible} />
+              <p>Public</p>
+            </div>
           </div>
           <DroppableLocation id={windowId} locationId={windowId} fullSize={true}>
             <div className="flex flex-row">{children}</div>
--- app/serializers/abstract_file_serializer.rb
+++ app/serializers/abstract_file_serializer.rb
@@ -1,5 +1,5 @@
 class AbstractFileSerializer < ApplicationSerializer
   include JSONAPI::Serializer
-  attributes :id, :name, :path, :file_type, :size, :icon_url, :public_share_url
+  attributes :id, :name, :path, :file_type, :size, :icon_url, :public_share_url, :publicly_accessible
 
 end
