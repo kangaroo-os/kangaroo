0c38e479f3162fc149f40cfcff6cd63424fa2eab
Olivia Li <oliviali.ouyang@gmail.com>
2022-06-20 17:04:17 -0700

    fix up the layout background files

--- app/javascript/components/ShareView.tsx
+++ app/javascript/components/ShareView.tsx
@@ -2,20 +2,16 @@ import React, { useEffect, useRef, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import api from '@helpers/api'
 import FileIcon from './shared/FileIcon'
+import { AxiosResponse } from 'axios'
 
 export const ShareView = () => {
   let { share_id } = useParams()
-  let imgRef = useRef<HTMLImageElement>()
   const [files, setFiles] = useState([])
   useEffect(() => {
     if (share_id) {
-      api.get(`/files/proxied_files/${share_id}`).then((res) => {
+      api.get(`/files/proxied_files/${share_id}`).then((res: AxiosResponse) => {
         if (res.data.files) {
           setFiles(res.data.files)
-        } else if (res.data) {
-          let binaryData = []
-          binaryData.push(res.data)
-          imgRef.current.src = window.URL.createObjectURL(new Blob(binaryData, { type: 'application/png' }))
         }
       })
     }
@@ -27,13 +23,11 @@ export const ShareView = () => {
     })
   }
 
-  function renderFilePreview() {
-    return <img ref={imgRef} />
-  }
   return (
-    <div className="m-24">
-      {files.length > 1 && <div className="shadow-lg w-full">{files.length > 0 && renderFiles()}</div>}
-      {files.length <= 1 && renderFilePreview()}
+    <div className="m-24 ">
+      {files.length > 0 && (
+        <div className="border-gray-400 rounded-lg z-10 bg-gray-200 shadow-lg m-auto max-w-[800px] p-5">{files.length > 0 && renderFiles()}</div>
+      )}
     </div>
   )
 }
